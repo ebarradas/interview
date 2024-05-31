41c7e5460e8282ab92c09e0f093a377275157fb7
Jorge de Jesús Martinez Espino <jorgedejesusmartinezespino@mbp-de-jorge.lan>
2024-05-31 10:30:17 -0600

    Remove node_modules

--- index.js
+++ index.js
@@ -2,26 +2,37 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8081;
+const cars = require('./cars.js');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ ok: true });
-});
 
 app.get('/cars', (req, res) => {
   res.json(cars);
 });
 
+app.get('/', (req, res) => {
+  res.json({ ok: true });
+});
+
 // TODO: Add a route that receives an id and returns the car with that id
+app.get('/cars/:id', (req, res) => {
+  // use ejs documentation https://ejs.co/#docs
+  console.log('cars')
+  console.log(cars.cars)
+  console.log(req.params.id)
+ res.json(cars.cars.find((car) => car.id == req.params.id));
+});
+
 // TODO: Add a route that receives a make and returns all cars with that make
 
-app.get('/cars/list', (req, res) => {
+app.get('/cars/list/:make', (req, res) => {
     // use ejs documentation https://ejs.co/#docs
-    res.render('index', { cars: cars });
+
+    res.render('index', { cars: cars.cars.filter((car) => car.make === req.params.make) });
 });
 
 // Start the server
